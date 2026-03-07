2e8eb5b4ae96ed672bedf4b8cf05474a366f4be9
sean22-blip <iseandxd@gmail.com>
2026-03-07 12:11:00 +0700

    ready for module

--- react group_Project/src/App.jsx
+++ react group_Project/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Body from './components/Body'
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Header>
+    </Header>
   )
 }
 
